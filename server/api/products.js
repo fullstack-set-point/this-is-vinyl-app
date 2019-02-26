f04eff907e1d8535bbae9d8a8b5ccaf86a4fe059
Sandy Jones <sjonesmse@gmail.com>
2019-02-26 14:59:53 -0600

    products api routes and updated use router

---
+++ server/api/products.js
@@ -0,0 +1,16 @@
+const router = require('express').Router()
+const {Product} = require('../db/models')
+module.exports = router
+
+router.get('/', async (req, res, next) => {
+  try {
+    const products = await Product.findAll({
+      // explicitly select only the id and email fields - even though
+      // users' passwords are encrypted, it won't help if we just
+      // send everything to anyone who asks!
+    })
+    res.json(products)
+  } catch (err) {
+    next(err)
+  }
+})
