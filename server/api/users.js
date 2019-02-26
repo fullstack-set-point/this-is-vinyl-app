9c9ec7712f8f38f37f700efb3b932ac3d7aceeab
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-02-26 16:23:56 -0600

    added routes for users

--- server/api/users.js
+++ server/api/users.js
@@ -1,17 +1,59 @@
 const router = require('express').Router()
-const {User} = require('../db/models')
-module.exports = router
+const User = require('../db/models/User')
 
 router.get('/', async (req, res, next) => {
   try {
-    const users = await User.findAll({
-      // explicitly select only the id and email fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      attributes: ['id', 'email']
-    })
+    const users = await User.findAll({})
     res.json(users)
   } catch (err) {
     next(err)
   }
 })
+
+router.get('/:userId', async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.userId, {
+      include: [
+        // { model: Cart, as: 'cart' },
+        // { model: Order, as: 'orders' },
+        // { model: Review, as: 'reviews' }
+      ]
+    })
+    res.json(user)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const {email, password} = req.body
+    const user = await User.create(email, password)
+    res.json(user)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.put('/:userId', async (req, res, next) => {
+  try {
+    const user = await User.update(req.body, {
+      returning: true,
+      where: {id: req.params.userId}
+    })
+    res.json(user)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.delete('/:userId', async (req, res, next) => {
+  try {
+    await User.destroy({where: {id: req.params.userId}})
+    res.sendStatus(204)
+  } catch (err) {
+    next(err)
+  }
+})
+
+module.exports = router
