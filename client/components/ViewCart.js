6837a7364b0beff2b6f34b23b65b60546d8f4c4b
Sandy Jones <sjonesmse@gmail.com>
2019-02-27 16:02:10 -0600

    started ViewCart component, need to reseed

---
+++ client/components/ViewCart.js
@@ -0,0 +1,104 @@
+import React, {Component} from 'react'
+import {Table} from 'semantic-ui-react'
+import {connect} from 'react-redux'
+import {removeCartItem, fetchCartItems} from '../store/user'
+
+class ViewCart extends Component {
+  constructor(props) {
+    super(props)
+    this.handleRemove = this.handleRemove.bind(this)
+    // this.handleQuantityChange = this.handleQuantityChange.bind(this)
+  }
+
+  componentDidMount() {
+    this.props.fetchCartItems(this.props.match.params.userId)
+  }
+
+  // handleQuantityChange() {
+  //   this.props.changeQty()
+  // }
+
+  handleRemove() {
+    this.props.removeCartItem()
+  }
+
+  render() {
+    return (
+      <Table striped>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Name</Table.HeaderCell>
+            <Table.HeaderCell>Date Joined</Table.HeaderCell>
+            <Table.HeaderCell>E-mail</Table.HeaderCell>
+            <Table.HeaderCell>Called</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>John Lilki</Table.Cell>
+            <Table.Cell>September 14, 2013</Table.Cell>
+            <Table.Cell>jhlilk22@yahoo.com</Table.Cell>
+            <Table.Cell>No</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Jamie Harington</Table.Cell>
+            <Table.Cell>January 11, 2014</Table.Cell>
+            <Table.Cell>jamieharingonton@yahoo.com</Table.Cell>
+            <Table.Cell>Yes</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Jill Lewis</Table.Cell>
+            <Table.Cell>May 11, 2014</Table.Cell>
+            <Table.Cell>jilsewris22@yahoo.com</Table.Cell>
+            <Table.Cell>Yes</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>John Lilki</Table.Cell>
+            <Table.Cell>September 14, 2013</Table.Cell>
+            <Table.Cell>jhlilk22@yahoo.com</Table.Cell>
+            <Table.Cell>No</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>John Lilki</Table.Cell>
+            <Table.Cell>September 14, 2013</Table.Cell>
+            <Table.Cell>jhlilk22@yahoo.com</Table.Cell>
+            <Table.Cell>No</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Jamie Harington</Table.Cell>
+            <Table.Cell>January 11, 2014</Table.Cell>
+            <Table.Cell>jamieharingonton@yahoo.com</Table.Cell>
+            <Table.Cell>Yes</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Jill Lewis</Table.Cell>
+            <Table.Cell>May 11, 2014</Table.Cell>
+            <Table.Cell>jilsewris22@yahoo.com</Table.Cell>
+            <Table.Cell>Yes</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>John Lilki</Table.Cell>
+            <Table.Cell>September 14, 2013</Table.Cell>
+            <Table.Cell>jhlilk22@yahoo.com</Table.Cell>
+            <Table.Cell>No</Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {album: state.album}
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchCartItems: userId => dispatch(fetchCartItems(userId)),
+    // changeQty: () => dispatch(changeQty()),
+    removeCartItem: () => dispatch(removeCartItem())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ViewCart)
