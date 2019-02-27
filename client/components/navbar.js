8a21aeda0d7035273a1b5a1c65ac93379fcfca87
Tori Pope <toricpope@gmail.com>
2019-02-27 12:40:31 -0600

    added styling to allAlbums

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,7 +6,7 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1>BOILERMAKER</h1>
+    <h1>This is Vinyl App</h1>
     <nav>
       {isLoggedIn ? (
         <div>
